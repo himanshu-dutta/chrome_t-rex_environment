97302e8398715599d25452cbbe71ae8a1303e3bb
Himanshu Dutta <40473118+moriarity101@users.noreply.github.com>
2019-10-10 15:45:16 +0530

    Add files via upload

---
+++ chrome-trex/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='chrome_trex',
+      version='0.0.1',
+      install_requires=['gym','numpy','pygame']  # And any other dependencies Chrome-Trex Env needs
+)
